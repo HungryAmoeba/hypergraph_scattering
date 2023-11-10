7a3d9ba39ec6da5d444bca72f84b293a18267979
HungryAmoeba <charles.xu@yale.edu>
2023-11-10 12:23:48 -0500

    working hypergraph scattering network. need to do experiments

--- experiments/synthetic_hypergraph.py
+++ experiments/synthetic_hypergraph.py
@@ -4,4 +4,9 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-if __name__ == main:
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate synthetic hypergraph data')
+    parser.add_argument('--N', type=int, help = 'number of graphs to generate')
+    parser.add_argument('--type', type=str, options=['HSBM', ''])
+    parser.add_argument
+
