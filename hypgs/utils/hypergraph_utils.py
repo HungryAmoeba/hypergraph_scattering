17a24f9ce9b19a32b84173a309b19f01440c29bd
edebrouwer <edward.debrouwer@gmail.com>
2023-11-22 17:16:17 -0500

    clique to hypergraph utils

--- hypgs/utils/hypergraph_utils.py
+++ hypgs/utils/hypergraph_utils.py
@@ -3,6 +3,10 @@ import dhg
 import torch
 from torch_geometric.utils import from_networkx
 
+import torch_geometric
+import tqdm
+
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -22,6 +26,54 @@ def data_to_hg(data, add_k_hop = 0, min_k_hop_size = 0):
             edges_as_tuples = edges_as_tuples + hyperedges[0]
     hg = dhg.Hypergraph(data.num_nodes, edges_as_tuples)
     return hg
+
+def get_cliques_planar(graph):
+    """
+    Computes the 3 and 4-cliques from a torch geometric graph.
+
+    Input: 
+        - torch geometric graph
+    Output:
+        Same object with attributes "three_cliques" and "four_cliques"
+        - torch tensor of shape (3, num_3_cliques) containing the 3-cliques
+        - torch tensor of shape (4, num_4_cliques) containing the 4-cliques
+    """
+    four_cliques_list = []
+    three_cliques_list = []
+    for node_idx in tqdm.tqdm(range(graph.x.shape[0])):
+
+        subnodes, subgraph_edges, _, _ = torch_geometric.utils.k_hop_subgraph(node_idx = node_idx, num_hops = 1, edge_index = graph.edge_index)
+        periph_nodes = subnodes[subnodes != node_idx]
+        periph_edge_index, _ = torch_geometric.utils.subgraph(periph_nodes, subgraph_edges)
+        mask = periph_edge_index[0] < periph_edge_index[1]
+        periph_edge_index = periph_edge_index[:, mask]
+        
+        three_cliques = torch.cat([periph_edge_index, torch.ones((1,periph_edge_index.shape[1]), dtype = torch.long) * node_idx], dim = 0)
+        three_cliques_list.append(three_cliques)
+
+        four_cliques = []
+        #finding 4 cliques - just triangles in the peripherical nodes
+        if len(periph_nodes) >= 3:
+            for edge_ in periph_edge_index.T:
+                for node in periph_nodes:
+                    if node in edge_:
+                        continue # the node is already in the edge, not a triangle.
+                    mask_1 = (periph_edge_index==node) + (periph_edge_index == edge_[0])
+                    mask_2 = (periph_edge_index==node) + (periph_edge_index == edge_[1]) 
+                    hyperedge_found = mask_1.all(0).any() * mask_2.all(0).any()
+                    if hyperedge_found:
+                        four_cliques.append(torch.cat([edge_, torch.tensor([node]), torch.tensor([node_idx])])[None,:])
+        
+        if len(four_cliques)>0:
+            four_cliques = torch.cat(four_cliques).T
+            four_cliques_list.append(four_cliques)
+
+    three_cliques = torch.cat(three_cliques_list, dim = 1)
+    four_cliques = torch.cat(four_cliques_list, dim = 1)
+
+    graph.three_cliques = three_cliques
+    graph.four_cliques = four_cliques
+    return graph
 
 if __name__ == '__main__': 
     # visualize converting an ER graph into a hypergraph with the desired features
