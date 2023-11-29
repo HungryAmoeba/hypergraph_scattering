64ce2f60aed45824c2f1ca8b3e95681e539adec0
edebrouwer <edward.debrouwer@gmail.com>
2023-11-29 14:50:11 -0500

    clique + found out that graph is not planar

--- experiments/spacegm_general.py
+++ experiments/spacegm_general.py
@@ -29,193 +29,195 @@ from hypgs.utils.hypergraph_utils import CliqueHyperEdgeTransform
 from hypgs.data.spacegm import CellularGraphDataset
 from hypgs.data.spacegm_transforms import AddCenterCellType, AddGraphLabel, FeatureMask 
 
-parser = argparse.ArgumentParser(description='Process scattering on a dataset')
 
-# Load the YAML file
-# with open('../hypg_scattering/utils/config.yaml', 'r') as file:
-#     config = yaml.safe_load(file)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process scattering on a dataset')
 
-
-print(sys.argv)
-n = len(sys.argv)
-if n != 5:
-    print('Error. Usage: python spacegm_general.py dataset_name delete_processed classifier_name scattering_type')
-    print("example: python spacegm_general.py upmc 1 svm asym")
-    exit()
-
-dataset_name = str(sys.argv[1])
-
-delete = bool(int(sys.argv[2]))
-classifier = str(sys.argv[3])
-
-if classifier not in {'svm', 'logistic'}:
-    print('supported classifiers: svm and logistic')
-    exit()
-
-print(f'Delete existing transform = {delete}')
-
-root_dir = os.path.join(DATA_DIR,"upmc")
-
-WAVELET_TYPE = 2
-MAX_SCALE = 5
-NUM_LAYERS = 3
-HIGHEST_MOMENT = 1
-AGGREGATION_TYPE = 'L1'
-
-scattering_type = str(sys.argv[4])
-print(scattering_type)
-if scattering_type not in {'blis' , 'asym'}:
-    print('supported scattering types: blip and asym')
-    exit()
-
-#scatterTransform = GraphScatteringTransform(scattering_type, WAVELET_TYPE, MAX_SCALE, NUM_LAYERS, HIGHEST_MOMENT)
-#scatterTransform = GraphScatteringTransform(scattering_type, WAVELET_TYPE, MAX_SCALE, NUM_LAYERS, AGGREGATION_TYPE)
-#transformations = T.Compose([scatterTransform])
-#transformations = None
-
-# perhaps it is necessary to delete pre_transform every time it is used?
-processed_data_dir = os.path.join(root_dir, f'tg_graph')
-if delete:
-    try:
-        shutil.rmtree(processed_data_dir)
-        print(f"Directory '{processed_data_dir}' successfully deleted.")
-    except FileNotFoundError:
-        print(f"Directory '{processed_data_dir}' does not exist.")
-    except PermissionError:
-        print(f"Permission denied. Unable to delete directory '{processed_data_dir}'.")
-    except Exception as e:
-        print(f"An error occurred while deleting directory '{processed_data_dir}': {str(e)}")
+    # Load the YAML file
+    # with open('../hypg_scattering/utils/config.yaml', 'r') as file:
+    #     config = yaml.safe_load(file)
 
 
-dataset_root = root_dir
-dataset_kwargs = {
-    'transform': [],
-    'pre_transform': [CliqueHyperEdgeTransform()], # removed the scattering transform atm
-    'pre_pre_transform': None,
-    'raw_folder_name': 'graphs',  # os.path.join(dataset_root, "graph") is the folder where we saved nx graphs
-    'processed_folder_name': 'tg_graph',  # processed dataset files will be stored here
-    'preprocessed_folder_name': 'tg_pre_graph',
-    #'node_features': ["cell_type", "SIZE", "biomarker_expression", "neighborhood_composition", "center_coord"],  # There are all the cellular features that we want the dataset to compute
-    'node_features': ["cell_type", "SIZE", "biomarker_expression", "center_coord"],  # There are all the cellular features that we want the dataset to compute
-    'edge_features': ["edge_type", "distance"],  # edge (cell pair) features
-    # this is usualy 3, but I using 0 to indicidate that the whole graph should be used
-    'subgraph_size': 0,  # indicating we want to sample 3-hop subgraphs from these regions (for training/inference), this is a core parameter for SPACE-GM.
-    'subgraph_source': 'on-the-fly',
-    'subgraph_allow_distant_edge': True,
-    'subgraph_radius_limit': 200.,
-}
+    print(sys.argv)
+    n = len(sys.argv)
+    if n != 5:
+        print('Error. Usage: python spacegm_general.py dataset_name delete_processed classifier_name scattering_type')
+        print("example: python spacegm_general.py upmc 1 svm asym")
+        exit()
 
-feature_kwargs = {
-    "biomarker_expression_process_method": "linear",
-    "biomarker_expression_lower_bound": 0,
-    "biomarker_expression_upper_bound": 18,
-    "neighborhood_size": 10,
-}
-dataset_kwargs.update(feature_kwargs)
+    dataset_name = str(sys.argv[1])
 
-dataset = CellularGraphDataset(dataset_root, **dataset_kwargs)
+    delete = bool(int(sys.argv[2]))
+    classifier = str(sys.argv[3])
+
+    if classifier not in {'svm', 'logistic'}:
+        print('supported classifiers: svm and logistic')
+        exit()
+
+    print(f'Delete existing transform = {delete}')
+
+    root_dir = os.path.join(DATA_DIR,"upmc")
+
+    WAVELET_TYPE = 2
+    MAX_SCALE = 5
+    NUM_LAYERS = 3
+    HIGHEST_MOMENT = 1
+    AGGREGATION_TYPE = 'L1'
+
+    scattering_type = str(sys.argv[4])
+    print(scattering_type)
+    if scattering_type not in {'blis' , 'asym'}:
+        print('supported scattering types: blip and asym')
+        exit()
+
+    #scatterTransform = GraphScatteringTransform(scattering_type, WAVELET_TYPE, MAX_SCALE, NUM_LAYERS, HIGHEST_MOMENT)
+    #scatterTransform = GraphScatteringTransform(scattering_type, WAVELET_TYPE, MAX_SCALE, NUM_LAYERS, AGGREGATION_TYPE)
+    #transformations = T.Compose([scatterTransform])
+    #transformations = None
+
+    # perhaps it is necessary to delete pre_transform every time it is used?
+    processed_data_dir = os.path.join(root_dir, f'tg_graph')
+    if delete:
+        try:
+            shutil.rmtree(processed_data_dir)
+            print(f"Directory '{processed_data_dir}' successfully deleted.")
+        except FileNotFoundError:
+            print(f"Directory '{processed_data_dir}' does not exist.")
+        except PermissionError:
+            print(f"Permission denied. Unable to delete directory '{processed_data_dir}'.")
+        except Exception as e:
+            print(f"An error occurred while deleting directory '{processed_data_dir}': {str(e)}")
 
 
-graph_label_file = os.path.join(dataset_root, "upmc_labels_renamed.csv")
-transformers = [
-    # `AddCenterCellType` will add `node_y` attribute to the subgraph for node-level prediction task
-    # In this task we will mask the cell type of the center cell and use its neighborhood to predict the true cell type
-    #AddCenterCellType(dataset),
-    
-    # `AddGraphLabel` will add `graph_y` and `graph_w` attributes to the subgraph for graph-level prediction task
-    AddGraphLabel(graph_label_file, tasks=['survival_status']),
-    
-    # Transformer `FeatureMask` will zero mask all feature items not included in its argument
-    # In this tutorial we perform training/inference using cell types and center cell's size feature
-    #FeatureMask(dataset, use_center_node_features=['cell_type', 'SIZE'], use_neighbor_node_features=['cell_type']),
-]
-dataset.set_transforms(transformers)
+    dataset_root = root_dir
+    dataset_kwargs = {
+        'transform': [],
+        'pre_transform': [CliqueHyperEdgeTransform()], # removed the scattering transform atm
+        'pre_pre_transform': None,
+        'raw_folder_name': 'graphs',  # os.path.join(dataset_root, "graph") is the folder where we saved nx graphs
+        'processed_folder_name': 'tg_graph',  # processed dataset files will be stored here
+        'preprocessed_folder_name': 'tg_pre_graph',
+        #'node_features': ["cell_type", "SIZE", "biomarker_expression", "neighborhood_composition", "center_coord"],  # There are all the cellular features that we want the dataset to compute
+        'node_features': ["cell_type", "SIZE", "biomarker_expression", "center_coord"],  # There are all the cellular features that we want the dataset to compute
+        'edge_features': ["edge_type", "distance"],  # edge (cell pair) features
+        # this is usualy 3, but I using 0 to indicidate that the whole graph should be used
+        'subgraph_size': 0,  # indicating we want to sample 3-hop subgraphs from these regions (for training/inference), this is a core parameter for SPACE-GM.
+        'subgraph_source': 'on-the-fly',
+        'subgraph_allow_distant_edge': True,
+        'subgraph_radius_limit': 200.,
+    }
 
-breakpoint()
+    feature_kwargs = {
+        "biomarker_expression_process_method": "linear",
+        "biomarker_expression_lower_bound": 0,
+        "biomarker_expression_upper_bound": 18,
+        "neighborhood_size": 10,
+    }
+    dataset_kwargs.update(feature_kwargs)
 
-print(f'finished processing {dataset_name}')
-import pdb; pdb.set_trace()
-print("modeling using logistic regression")
-n_splits = 5
-print(f"training with {n_splits}-fold")
+    dataset = CellularGraphDataset(dataset_root, **dataset_kwargs)
 
-if classifier == 'logistic':
-    model = make_pipeline(StandardScaler(), LogisticRegression(max_iter = 1000))
-    print('initialized logistic regression')
-else:
-    model = make_pipeline(StandardScaler(), SVC(kernel='rbf'))
-    print('initialized SVM with RBF kernel')
-kf = KFold(n_splits = n_splits)
-loader = DataLoader(dataset, batch_size = 1, shuffle = True)
 
-# Perform k-fold cross-validation
-fold_scores = []
-fold_accuracies = []
-fold_precisions = []
-fold_recalls = []
-fold_f1_scores = []
-#import pdb; pdb.set_trace()
-#dataset.x = torch.nan_to_num(dataset.x)
-for train_indices, test_indices in kf.split(dataset):
-    # Split the dataset into train and test folds
-    train_data = [dataset[train_idx] for train_idx in train_indices]
-    test_data = [dataset[test_idx] for test_idx in test_indices]
+    graph_label_file = os.path.join(dataset_root, "upmc_labels_renamed.csv")
+    transformers = [
+        # `AddCenterCellType` will add `node_y` attribute to the subgraph for node-level prediction task
+        # In this task we will mask the cell type of the center cell and use its neighborhood to predict the true cell type
+        #AddCenterCellType(dataset),
+        
+        # `AddGraphLabel` will add `graph_y` and `graph_w` attributes to the subgraph for graph-level prediction task
+        AddGraphLabel(graph_label_file, tasks=['survival_status']),
+        
+        # Transformer `FeatureMask` will zero mask all feature items not included in its argument
+        # In this tutorial we perform training/inference using cell types and center cell's size feature
+        #FeatureMask(dataset, use_center_node_features=['cell_type', 'SIZE'], use_neighbor_node_features=['cell_type']),
+    ]
+    dataset.set_transforms(transformers)
 
-    train_dat_x = torch.cat([d.x for d in train_data])
-    train_dat_y = torch.cat([d.graph_y for d in train_data])[:,0]
+    breakpoint()
 
-    test_dat_x = torch.cat([d.x for d in test_data])
-    test_dat_y = torch.cat([d.graph_y for d in test_data])[:,0]
+    print(f'finished processing {dataset_name}')
+    import pdb; pdb.set_trace()
+    print("modeling using logistic regression")
+    n_splits = 5
+    print(f"training with {n_splits}-fold")
 
-    # Train the logistic regression model
-    model.fit(train_dat_x, train_dat_y)
+    if classifier == 'logistic':
+        model = make_pipeline(StandardScaler(), LogisticRegression(max_iter = 1000))
+        print('initialized logistic regression')
+    else:
+        model = make_pipeline(StandardScaler(), SVC(kernel='rbf'))
+        print('initialized SVM with RBF kernel')
+    kf = KFold(n_splits = n_splits)
+    loader = DataLoader(dataset, batch_size = 1, shuffle = True)
 
-    # Make predictions on the test fold
-    y_pred = model.predict(test_dat_x)
+    # Perform k-fold cross-validation
+    fold_scores = []
+    fold_accuracies = []
+    fold_precisions = []
+    fold_recalls = []
+    fold_f1_scores = []
+    #import pdb; pdb.set_trace()
+    #dataset.x = torch.nan_to_num(dataset.x)
+    for train_indices, test_indices in kf.split(dataset):
+        # Split the dataset into train and test folds
+        train_data = [dataset[train_idx] for train_idx in train_indices]
+        test_data = [dataset[test_idx] for test_idx in test_indices]
 
-    # Calculate evaluation metrics
-    accuracy = accuracy_score(test_dat_y, y_pred)
-    precision = precision_score(test_dat_y, y_pred, average='macro')
-    recall = recall_score(test_dat_y, y_pred, average='macro')
-    f1 = f1_score(test_dat_y, y_pred, average='macro')
+        train_dat_x = torch.cat([d.x for d in train_data])
+        train_dat_y = torch.cat([d.graph_y for d in train_data])[:,0]
 
-    # Append scores and metrics to lists
-    fold_scores.append(model.score(test_dat_x, test_dat_y))
-    fold_accuracies.append(accuracy)
-    fold_precisions.append(precision)
-    fold_recalls.append(recall)
-    fold_f1_scores.append(f1)
+        test_dat_x = torch.cat([d.x for d in test_data])
+        test_dat_y = torch.cat([d.graph_y for d in test_data])[:,0]
 
-# Calculate mean and standard deviation of scores
-mean_score = sum(fold_scores) / n_splits
-std_score = torch.tensor(fold_scores).std().item()
+        # Train the logistic regression model
+        model.fit(train_dat_x, train_dat_y)
 
-# Calculate mean and standard deviation of metrics
-mean_accuracy = sum(fold_accuracies) / n_splits
-std_accuracy = torch.tensor(fold_accuracies).std().item()
-mean_precision = sum(fold_precisions) / n_splits
-std_precision = torch.tensor(fold_precisions).std().item()
-mean_recall = sum(fold_recalls) / n_splits
-std_recall = torch.tensor(fold_recalls).std().item()
-mean_f1_score = sum(fold_f1_scores) / n_splits
-std_f1_score = torch.tensor(fold_f1_scores).std().item()
+        # Make predictions on the test fold
+        y_pred = model.predict(test_dat_x)
 
-# Print scores and performance metrics
-print(f"Cross-Validation Scores: {fold_scores}")
-print(f"Mean Score: {mean_score:.4f} ± {std_score:.4f}")
-print("")
+        # Calculate evaluation metrics
+        accuracy = accuracy_score(test_dat_y, y_pred)
+        precision = precision_score(test_dat_y, y_pred, average='macro')
+        recall = recall_score(test_dat_y, y_pred, average='macro')
+        f1 = f1_score(test_dat_y, y_pred, average='macro')
 
-print(f"Accuracy Scores: {fold_accuracies}")
-print(f"Mean Accuracy: {mean_accuracy:.4f} ± {std_accuracy:.4f}")
-print("")
+        # Append scores and metrics to lists
+        fold_scores.append(model.score(test_dat_x, test_dat_y))
+        fold_accuracies.append(accuracy)
+        fold_precisions.append(precision)
+        fold_recalls.append(recall)
+        fold_f1_scores.append(f1)
 
-print(f"Precision Scores: {fold_precisions}")
-print(f"Mean Precision: {mean_precision:.4f} ± {std_precision:.4f}")
-print("")
+    # Calculate mean and standard deviation of scores
+    mean_score = sum(fold_scores) / n_splits
+    std_score = torch.tensor(fold_scores).std().item()
 
-print(f"Recall Scores: {fold_recalls}")
-print(f"Mean Recall: {mean_recall:.4f} ± {std_recall:.4f}")
-print("")
+    # Calculate mean and standard deviation of metrics
+    mean_accuracy = sum(fold_accuracies) / n_splits
+    std_accuracy = torch.tensor(fold_accuracies).std().item()
+    mean_precision = sum(fold_precisions) / n_splits
+    std_precision = torch.tensor(fold_precisions).std().item()
+    mean_recall = sum(fold_recalls) / n_splits
+    std_recall = torch.tensor(fold_recalls).std().item()
+    mean_f1_score = sum(fold_f1_scores) / n_splits
+    std_f1_score = torch.tensor(fold_f1_scores).std().item()
 
-print(f"F1 Scores: {fold_f1_scores}")
-print(f"Mean F1 Score: {mean_f1_score:.4f} ± {std_f1_score:.4f}")
+    # Print scores and performance metrics
+    print(f"Cross-Validation Scores: {fold_scores}")
+    print(f"Mean Score: {mean_score:.4f} ± {std_score:.4f}")
+    print("")
+
+    print(f"Accuracy Scores: {fold_accuracies}")
+    print(f"Mean Accuracy: {mean_accuracy:.4f} ± {std_accuracy:.4f}")
+    print("")
+
+    print(f"Precision Scores: {fold_precisions}")
+    print(f"Mean Precision: {mean_precision:.4f} ± {std_precision:.4f}")
+    print("")
+
+    print(f"Recall Scores: {fold_recalls}")
+    print(f"Mean Recall: {mean_recall:.4f} ± {std_recall:.4f}")
+    print("")
+
+    print(f"F1 Scores: {fold_f1_scores}")
+    print(f"Mean F1 Score: {mean_f1_score:.4f} ± {std_f1_score:.4f}")
--- hypgs/utils/hypergraph_utils.py
+++ hypgs/utils/hypergraph_utils.py
@@ -7,6 +7,12 @@ from torch_geometric.transforms import BaseTransform
 import torch_geometric
 import tqdm
 
+from functools import partial
+
+from multiprocessing import Pool
+
+import torch.multiprocessing as mp
+
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -28,6 +34,66 @@ def data_to_hg(data, add_k_hop = 0, min_k_hop_size = 0):
     hg = dhg.Hypergraph(data.num_nodes, edges_as_tuples)
     return hg
 
+def get_clique_from_node( node_idx, graph ):
+    subnodes, subgraph_edges, _, _ = torch_geometric.utils.k_hop_subgraph(node_idx = node_idx, num_hops = 1, edge_index = graph.edge_index)
+    periph_nodes = subnodes[subnodes != node_idx]
+    periph_edge_index, _ = torch_geometric.utils.subgraph(periph_nodes, subgraph_edges)
+    mask = periph_edge_index[0] < periph_edge_index[1]
+    periph_edge_index = periph_edge_index[:, mask]
+    
+    three_cliques = torch.cat([periph_edge_index, torch.ones((1,periph_edge_index.shape[1]), dtype = torch.long) * node_idx], dim = 0)
+
+    four_cliques = []
+    #finding 4 cliques - just triangles in the peripherical nodes
+    if len(periph_nodes) >= 3:
+        for edge_ in periph_edge_index.T:
+            for node in periph_nodes:
+                if node in edge_:
+                    continue # the node is already in the edge, not a triangle.
+                mask_1 = (periph_edge_index==node) + (periph_edge_index == edge_[0])
+                mask_2 = (periph_edge_index==node) + (periph_edge_index == edge_[1]) 
+                hyperedge_found = mask_1.all(0).any() * mask_2.all(0).any()
+                if hyperedge_found:
+                    four_cliques.append(torch.cat([edge_, torch.tensor([node]), torch.tensor([node_idx])])[None,:])
+    
+    if len(four_cliques)>0:
+        four_cliques = torch.cat(four_cliques).T
+    
+    return (three_cliques, four_cliques)
+
+def get_cliques_planar_(graph, njobs = 5):
+    """
+    Computes the 3 and 4-cliques from a torch geometric graph.
+
+    Input: 
+        - torch geometric graph
+    Output:
+        Same object with attributes "three_cliques" and "four_cliques"
+        - torch tensor of shape (3, num_3_cliques) containing the 3-cliques
+        - torch tensor of shape (4, num_4_cliques) containing the 4-cliques
+    """
+    four_cliques_list = []
+    three_cliques_list = []
+
+    f = partial(get_clique_from_node, graph = graph)
+
+    with Pool(njobs) as p:
+        res = p.map(f,[i for i in range(graph.x.shape[0])])
+
+    three_cliques_comb = torch.cat([r[0] for r in res], dim = 1)
+    four_cliques_comb = torch.cat([r[1] for r in res if len(r[1])>0], dim = 1)
+
+    three_cliques_sorted = torch.sort(three_cliques_comb,0)[0]
+    four_cliques_sorted = torch.sort(four_cliques_comb,0)[0]
+
+    three_cliques = torch.unique(three_cliques_sorted, sorted = False, dim = 1)
+    four_cliques = torch.unique(four_cliques_sorted, sorted = False, dim = 1)
+
+    graph.three_cliques = three_cliques
+    graph.four_cliques = four_cliques
+
+    return graph
+
 def get_cliques_planar(graph, njobs = 1):
     """
     Computes the 3 and 4-cliques from a torch geometric graph.
@@ -41,6 +107,8 @@ def get_cliques_planar(graph, njobs = 1):
     """
     four_cliques_list = []
     three_cliques_list = []
+
+
     for node_idx in tqdm.tqdm(range(graph.x.shape[0])):
 
         subnodes, subgraph_edges, _, _ = torch_geometric.utils.k_hop_subgraph(node_idx = node_idx, num_hops = 1, edge_index = graph.edge_index)
@@ -54,6 +122,7 @@ def get_cliques_planar(graph, njobs = 1):
 
         four_cliques = []
         #finding 4 cliques - just triangles in the peripherical nodes
+        
         if len(periph_nodes) >= 3:
             for edge_ in periph_edge_index.T:
                 for node in periph_nodes:
@@ -63,6 +132,7 @@ def get_cliques_planar(graph, njobs = 1):
                     mask_2 = (periph_edge_index==node) + (periph_edge_index == edge_[1]) 
                     hyperedge_found = mask_1.all(0).any() * mask_2.all(0).any()
                     if hyperedge_found:
+                        breakpoint()
                         four_cliques.append(torch.cat([edge_, torch.tensor([node]), torch.tensor([node_idx])])[None,:])
         
         if len(four_cliques)>0:
@@ -77,7 +147,7 @@ def get_cliques_planar(graph, njobs = 1):
 
     graph.three_cliques = three_cliques
     graph.four_cliques = four_cliques
-    
+
     return graph
 
 class CliqueHyperEdgeTransform(BaseTransform):
